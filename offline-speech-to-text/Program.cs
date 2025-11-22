963cb6d2bde841208251da2f4ebbfafc374f47fe
gladys6-arch <gladys6-arch@github.com>
2025-11-22 11:26:18 +0300

    display results

--- offline-speech-to-text/Program.cs
+++ offline-speech-to-text/Program.cs
@@ -8,7 +8,7 @@ class.Program
   {
     // path for Vosk Model
     string modelPath = "models/vosk-model-small-en-us-0.15";
-    //pat for wav file
+    //path for wav file
     string WAVPath = "audio/test.wav";
 
     // intialize Vosk model
@@ -19,7 +19,31 @@ class.Program
 
 
 
+     4. Read the WAV file and feed it to the recognizer
+      
+        // We read the file in small chunks of 4096 bytes
+        using (FileStream fs = new FileStream(wavPath, FileMode.Open))
+        {
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+
+            while ((bytesRead = fs.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                // Send the audio chunk to the recognizer
+                recognizer.AcceptWaveform(buffer, bytesRead);
+            }
+        }  
+
+
+         // 5. Get the final recognized text
         
+        string result = recognizer.FinalResult();
+
+        // -------------------------------
+        // 6. Display the result
+        
+        Console.WriteLine("Recognized text:");
+        Console.WriteLine(result); 
 
 
   }
