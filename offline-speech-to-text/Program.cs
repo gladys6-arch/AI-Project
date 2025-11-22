f0137d3e2f9387f84fe59ca3f50659585d2ed3f7
gladys6-arch <gladys6-arch@github.com>
2025-11-22 11:10:30 +0300

    Add offline speech-to-text project files

---
+++ offline-speech-to-text/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using Vosk;
+
+class.Program
+{
+  static void Main(string[] args)
+  {
+    // paths
+    string modelPath = "models/vosk-model-small-en-us-0.15";
+    string audioFolder = "audio";
+  }
+}
